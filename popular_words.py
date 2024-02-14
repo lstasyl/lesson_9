f93cfada4e319e4995a0114e4d35c7e84de1c323
_istasyi_ <156712861+lstasyl@users.noreply.github.com>
2024-02-14 08:39:56 +0200

    Add files via upload

---
+++ popular_words.py
@@ -0,0 +1,15 @@
+def popular_words(text, words):
+    txt_lower = text.lower()
+    wrd_dict = {}
+
+    for word in words:
+        if len(word) == 1:
+            wrd_dict[word] = txt_lower.count(" " + word + " ")
+        else:
+            wrd_dict[word] = txt_lower.split().count(word)
+
+    return wrd_dict
+
+assert popular_words('''Hi! When I was One I had just begun When I was Two I was nearly new ''',
+                     ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1'
+print('OK')
